fc62dd6f3a6c91cee7960989dd6dbe5842f27949
taylor-w <taylorwood39@gmail.com>
2020-11-12 19:50:50 -0600

    Add example of 'setInterval()'

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import https from "https";
 
-const req = https.request(
-  {
-    hostname: "google.com",
-    port: 443,
-    method: "GET",
-    path: "/",
-  },
-  (res) => {
-    console.log(res.statusCode);
-    res.on("data", (data) => {
-      console.log(data);
-    });
+setInterval(() => {
+  const req = https.request(
+    {
+      hostname: "google.com",
+      port: 443,
+      method: "GET",
+      path: "/",
+    },
+    (res) => {
+      console.log(res.statusCode);
+      res.on("data", (data) => {
+        console.log(data);
+      });
 
-    res.on("error", (error) => {
-      console.error(error);
-    });
-  }
-);
+      res.on("error", (error) => {
+        console.error(error);
+      });
+    }
+  );
 
-req.end();
+  req.end();
+}, 2000);
