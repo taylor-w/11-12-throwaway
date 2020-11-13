3855002e8a32940a402c45ced43deeeb1e6f1b66
taylor-w <taylorwood39@gmail.com>
2020-11-12 18:56:39 -0600

    Add simple example of basic async event and callback

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// this only works because of webpack
+// normally, node only uses required
+// i.e.) const fs = require('fs')
+import fs from "fs";
+
+fs.readFile(__filename, "utf8", (err, results) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(results);
+  }
+});
