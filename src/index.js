27252e25c6128a6315ee383efa655eec3736ef23
taylor-w <taylorwood39@gmail.com>
2020-11-12 20:42:21 -0600

    Read file and chain then using promises and save result as 'test.txt'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// clear index.js
+// destructure 'promises' from 'fs' object and name it 'as fs'.
+import { promises as fs, promises } from "fs";
+
+fs.readFile(__filename)
+  .then((results) => {
+    fs.writeFile("test.txt", results);
+  })
+  .then(() => {
+    console.log("file written");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
