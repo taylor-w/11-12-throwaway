f3a9be80cb753d9cea865c1bb2694fa74cac776e
Taylor Wood <58274421+taylor-w@users.noreply.github.com>
2020-11-12 18:35:42 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:node/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  rules: {
+    "array-callback-return": "warn",
+    "import/no-unresolved": [
+      1,
+      {
+        ignore: [
+          // TODO: Add additional directories that you may want to use for 'absolute imports'
+          "api",
+          "db",
+          "lib",
+        ],
+      },
+    ],
+    "import/prefer-default-export": 1,
+    "no-debugger": "warn",
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "no-unused-vars": ["warn", {
+      argsIgnorePattern: "^_",
+      varsIgnorePattern: "^_" }],
+    "node/no-unsupported-features/es-syntax": ["warn", {
+      "ignores": ["modules"]
+    }]
+  },
+};
